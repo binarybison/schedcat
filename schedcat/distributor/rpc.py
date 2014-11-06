a5cd21e84fb86dd2b77fdcffa4e4941132f9bf8e
Bryan Ward <bcw@cs.unc.edu>
2014-11-06 14:10:50 -0500

    Minor fix.
    Must have merged some code wrong.

--- schedcat/distributor/rpc.py
+++ schedcat/distributor/rpc.py
@@ -294,7 +294,7 @@ class ExperimentManager(pb.Root):
 
     def remote_record_data(self, dp, trials, elapsed, results):
         dpo = self.outstanding[dict_to_key(dp)]
-        dpo.save(trials, elapsed, results)
+        dpo.update(trials, elapsed, results)
 
         del(self.outstanding[dict_to_key(dp)])
 
